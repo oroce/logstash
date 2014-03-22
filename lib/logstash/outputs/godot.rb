76a503ea26ea691d9ee4f05498625073a0340ca2
oroce <robert@oroszi.net>
2014-03-22 16:01:11 +0100

    initial version of godot output, both tcp and udp working

---
+++ lib/logstash/outputs/godot.rb
@@ -0,0 +1,165 @@
+# encoding: utf-8
+require "logstash/outputs/base"
+require "logstash/namespace"
+require "json"
+require "socket"
+class LogStash::Outputs::Godot < LogStash::Outputs::Base
+  config_name "godot"
+  milestone 1
+
+  # The address of the Riemann server.
+  config :host, :validate => :string, :default => "localhost"
+
+  # The port to connect to on your Riemann server.
+  config :port, :validate => :number, :default => 1337
+
+  # The protocol to use
+  # UDP is non-blocking
+  # TCP is blocking
+  #
+  # Logstash's default output behaviour
+  # is to never lose events
+  # As such, we use tcp as default here
+  config :protocol, :validate => ["tcp", "udp"], :default => "tcp"
+
+  # The name of the sender.
+  # This sets the `host` value
+  # in the Riemann event
+  config :sender, :validate => :string, :default => "%{host}"
+
+  # A Hash to set Riemann event fields
+  # (<http://riemann.io/concepts.html>).
+  #
+  # The following event fields are supported:
+  # `description`, `state`, `metric`, `ttl`, `service`
+  #
+  # Tags found on the Logstash event will automatically be added to the
+  # Riemann event.
+  #
+  # Any other field set here will be passed to Riemann as an event attribute.
+  #
+  # Example:
+  #
+  #     riemann {
+  #         riemann_event => {
+  #             "metric"  => "%{metric}"
+  #             "service" => "%{service}"
+  #         }
+  #     }
+  #
+  # `metric` and `ttl` values will be coerced to a floating point value.
+  # Values which cannot be coerced will zero (0.0).
+  #
+  # `description`, by default, will be set to the event message
+  # but can be overridden here.
+  config :godot_event, :validate => :hash
+
+  # If set to true automatically map all logstash defined fields to riemann event fields.
+  # All nested logstash fields will be mapped to riemann fields containing all parent keys
+  # separated by dots and the deepest value.
+  #
+  # As an example, the logstash event:
+  #    {
+  #      "@timestamp":"2013-12-10T14:36:26.151+0000",
+  #      "@version": 1,
+  #      "message":"log message",
+  #      "host": "host.domain.com",
+  #      "nested_field": {
+  #                        "key": "value"
+  #                      }
+  #    }
+  # Is mapped to this riemann event:
+  #   {
+  #     :time 1386686186,
+  #     :host host.domain.com,
+  #     :message log message,
+  #     :nested_field.key value
+  #   }
+  #
+  # It can be used in conjunction with or independent of the riemann_event option.
+  # When used with the riemann_event any duplicate keys receive their value from
+  # riemann_event instead of the logstash event itself.
+  config :map_fields, :validate => :boolean, :default => false
+
+  #
+  # Enable debugging output?
+  config :debug, :validate => :boolean, :default => false
+
+  public
+  def register
+    connect
+    @logger.warn( "socket is initialized with", :protocol => @protocol )
+  end # def register
+
+  public
+  def connect
+    if @protocol == "udp"
+      @socket = UDPSocket.new
+    else
+      @socket = TCPSocket.new @host, @port
+    end
+  end # def connect
+
+  public
+  def map_fields(parent, fields)
+    fields.each {|key, val|
+      if !key.start_with?("@")
+        field = parent.nil? ? key : parent + '.' + key
+        contents = val                            
+        if contents.is_a?(Hash)                                     
+          map_fields(field, contents)                                       
+        else                                                                                  
+          @my_event[field.to_sym] = contents                                                          
+        end
+      end
+    }                 
+  end
+
+  public
+  def receive(event)
+    @logger.warn( "got the event", :event => event )
+    return unless output?(event)
+
+    # Let's build us an event, shall we?
+    payload = Hash.new
+    payload[:host] = event.sprintf(@sender)
+    
+    payload[:time] = event["@timestamp"].to_i
+    payload[:description] = event["message"]
+    if @godot_event
+      @godot_event.each do |key, val|
+        if ["ttl","metric"].include?(key)
+          payload[key.to_sym] = event.sprintf(val).to_f
+        else
+          payload[key.to_sym] = event.sprintf(val)
+        end
+      end
+    end
+    if @map_fields == true
+      @my_event = Hash.new
+      map_fields(nil, event)
+      payload.merge!(@my_event) {|key, val1, val2| val1}
+    end
+    payload[:tags] = event["tags"] if event["tags"].is_a?(Array)
+    @logger.debug("Godot event: ", :godot_event => payload)
+    send(payload.to_json)
+  end # def receive
+
+  public
+  def send(data)
+    if @protocol == "udp"
+      begin
+        @socket.send(data, 0, @host, @port);
+      rescue Exception => e
+        @logger.warn("Unhandled exception in udp", :error => e)
+      end
+    else
+      begin
+        @socket.puts(data)
+      rescue Exception => e
+        @logger.warn("Unhandled exception in tcp", :error => e)
+      end
+    end
+  end # def send
+
+end # class LogStash::Outputs::Godot
